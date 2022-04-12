3dc53f96d2a198c110e0831461aeafdbac5140a8
zaeem@kavano <zaeem@kavano.co>
2022-04-12 15:23:16 +0430

    rank incompletedo

--- src/controllers/GradeController.py
+++ src/controllers/GradeController.py
@@ -6,35 +6,52 @@ from src.models.User import User
 from src.database.Database import Database
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt, get_jwt_identity
 import numpy as np
+import pandas
 
 
 @jwt_required()
 def insert():
 
     json = request.get_json()
+    users, weights = np.array(json[0]["users"]), np.array(json[0]["weights"])
 
-    users, weights = np.array(json[0].get(
-        "users")), np.array(json[0].get("weights"))
-    users = json[0].get(
-        "users")
-    print("*********************", "\n", "\n", "*********************", )
-    # print(users, "\n", weights)
-    print(np.array(users[0:2]))
-    print("*********************", "\n", "\n", "*********************", )
+    print(users, "\n", "||||||||||||||||||||||||||||||||||||||||||||||",
+          "\n")
+
+    # array = np.array([4, 2, 7, 1])
+    # temp = array.argsort()
+    # ranks = np.empty_like(temp)
+    # ranks[temp] = np.arange(len(array))
+    # print(array, "\n", ranks)
+    math_marks = []
     for courses in users:
-        # print(courses["name"])
-        numpy_2d_arrays = np.array([courses['name'], courses["courses"]])
-        # numpy_2d_arrays = arr.append(arr)
-
-    # print(numpy_2d_arrays)
+        scores = courses[4]
+        # print(scores[0])
+        math_marks.append(scores[0])
     current_rank = 0
     global_rank = 0
     current_mark = 0
+    print(math_marks)
+    for mark in math_marks:
+        global_rank += 1
+        if mark != current_rank:
+            current_mark = mark
+            current_rank = global_rank
+        print(current_mark, current_rank)
 
-    # print(json[0]["users"])
-    # print(numpy_2d_arrays)
-    # print(1111111111111)
-    return jsonify({"hell": "d"})
+##[18, 17, 20, 20, 17, 17, 18, 0]
+# 18 1
+# 17 2
+# 20 3
+# 20 4
+# 17 5
+# 17 6
+# 18 7
+# 0 8
+#
+#
+#
+        return jsonify({"hell": "d"})
 
 
 def pu():
